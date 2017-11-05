4ae95b2f2b17bbe6c7b3e7033317ca6d8f088b62
yommi <tomvdalen20@gmail.com>
2017-11-05 10:59:27 +0100

    This should do something...

---
+++ Assets/googlemaps.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class googlemaps: MonoBehaviour {
+
+	public string pageToOpen = "https://www.google.com/maps/d/viewer?mid=105rOU1HDlmvLVb8sLAVcJ3-7SII&II=52.66232368194429%2C6.7391739176950000001&z=17";
+
+	// check readme file to find out how to change title, colors etc.
+	public void Onactivated() {
+		InAppBrowser.DisplayOptions options = new InAppBrowser.DisplayOptions();
+		options.displayURLAsPageTitle = false;
+		options.pageTitle = "InAppBrowser example";
+
+		InAppBrowser.OpenURL(pageToOpen, options);
+	}
+
+	public void OnClearCacheClicked() {
+		InAppBrowser.ClearCache();
+	}
+}
---
+++ Assets/website.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class website : MonoBehaviour {
+
+	public string pageToOpen = "stolpersteine.jimbo.com";
+
+	// check readme file to find out how to change title, colors etc.
+	public void Onactivated() {
+		InAppBrowser.DisplayOptions options = new InAppBrowser.DisplayOptions();
+		options.displayURLAsPageTitle = false;
+		options.pageTitle = "InAppBrowser example";
+
+		InAppBrowser.OpenURL(pageToOpen, options);
+	}
+
+	public void OnClearCacheClicked() {
+		InAppBrowser.ClearCache();
+	}
+}
